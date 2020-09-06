fc017df3825004381a50cc19ad012a5c92883005
AlexsandrRybalko <rybalkosanya20@gmail.com>
2020-09-06 11:33:59 +0300

    added UberCheaterPlayer

---
+++ Homework7ClassLibrary/UberCheaterPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Homework7ClassLibrary
+{
+    public class UberCheaterPlayer : Player
+    {
+        private List<int> NotUsedNumbers;
+        private int CurrentNumber = 40;
+        public override int GuessTheWeight()
+        {
+            while (NotUsedNumbers.Contains(CurrentNumber))
+            {
+                CurrentNumber += 1;
+            }
+            return CurrentNumber++;
+        }
+        public UberCheaterPlayer(string name, List<int> numbers)
+        {
+            Name = name;
+            NotUsedNumbers = numbers;
+        }
+    }
+}
